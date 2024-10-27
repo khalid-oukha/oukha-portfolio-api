8ab8fb905c4580655f2850630bcf43273e47840b
khalidwaheli <oukhakhalid@gmail.com>
2024-10-27 16:18:54 +0100

    added depandcies and connection to my portfolio database

---
+++ src/main/java/com/oukha/portfolio/api/OukhaPortfolioApiApplication.java
@@ -0,0 +1,13 @@
+package com.oukha.portfolio.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OukhaPortfolioApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OukhaPortfolioApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/oukha/portfolio/api/OukhaPortfolioApiApplicationTests.java
@@ -0,0 +1,15 @@
+package com.oukha.portfolio.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class OukhaPortfolioApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
